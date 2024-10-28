221c4e60e06666799bb5e50d9b4324ec3a259c49
kiu-chan <khanhk66uet@gmail.com>
2024-10-29 02:06:44 +0700

    up

--- lib/pagefour.dart
+++ lib/pagefour.dart
@@ -1,41 +1,39 @@
-// ignore_for_file: unused_import
-
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:async';
-import 'pagetest.dart';
 
 class pageFour extends StatefulWidget {
   const pageFour({super.key});
 
   @override
-  State<pageFour> createState() => _pageFourState();
+  State<pageFour> createState() => _pageFour();
 }
 
-StreamSocket streamSocket = StreamSocket();
-
-class _pageFourState extends State<pageFour> {
-  List<String> items = [
-    'Tài xế đã ngủ gật vào:         ',
-    'Tài xế đã ngủ gật vào:         ',
-    'Tài xế đã ngủ gật vào:         '
-  ];
+class _pageFour extends State<pageFour> {
+  List<String> items = []; // Start with an empty list
 
   Future refresh() async {
     final url = Uri.parse('http://14.225.211.5:5002');
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      final List newItems = json.decode(response.body);
-      setState(() {
-        items = newItems.map<String>((item) {
-          final number = item['thoigian'];
-
-          return '$number';
-        }).toList();
-      });
+  print("hello");
+    try {
+      final response = await http.get(url);
+      print('Response Body: ${response}');
+      if (response.statusCode == 200) {
+        final List<String> newItems = json.decode(response.body);
+        print('Response Body: ${response.body}');
+        print('Response Body: ${newItems}');
+        setState(() {
+          items.addAll(newItems
+              .map<String>((item) => 'Tài xế đã ngủ gật vào lúc: $item')
+              .toList());
+        });
+      } else {
+        // Error handling for unsuccessful HTTP status codes
+        debugPrint('Failed to fetch data: ${response.statusCode}');
+      }
+    } catch (e) {
+      // Handle any exceptions that occur during the HTTP request
+      print('Error occurred: $e');
     }
   }
 
@@ -57,7 +55,7 @@ class _pageFourState extends State<pageFour> {
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 25.0),
                     child: Text(
-                      'Lịch sử ',
+                      'Lịch sử ngủ gật',
                       style: TextStyle(
                         fontSize: 28,
                         fontWeight: FontWeight.bold,
